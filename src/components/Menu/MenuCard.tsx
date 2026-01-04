ab3d4753f5aff49e4cf3f29198e4eeca94f6fc84
Amer Bawji <amerbawji@Amers-MacBook-Pro.local>
2026-01-04 22:26:49 +0200

    Revert add button to full width

--- src/components/Menu/MenuCard.tsx
+++ src/components/Menu/MenuCard.tsx
@@ -201,7 +201,7 @@ export const MenuCard = ({ item }: MenuCardProps) => {
               </div>
               <Button 
                 onClick={handleAddToCart} 
-                className="self-end px-8 flex items-center justify-center gap-2"
+                className="w-full flex items-center justify-center gap-2"
                 aria-label={t('add')}
               >
                 <PlusIcon className="h-5 w-5" />
